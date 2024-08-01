b591f415fe97a6008e5bd6c878b0b15084ead3f1
AxelSalim <adjakidjememiaghe@gmail.com>
2024-08-01 17:14:04 +0100

    mise en place du header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,18 +8,21 @@ export default function Header() {
     
     
     const navigation = [
-      { title: "Features", path: "#" },
-      { title: "Integrations", path: "#" },
-      { title: "Customers", path: "#" },
-      { title: "Pricing", path: "#" },
+      { title: "Demos", path: "#" },
+      { title: "Pages", path: "#" },
+      { title: "Support", path: "#" },
+      { title: "Contact", path: "#" },
     ];
+
+
   return (
-    <section className="absolute z-20 inset-0">
+    <section className="">
+      <Image src="/1x/''Fichier 6.png" width={2000} height={2000} alt="Banner Image" className="absolute object-cover "/>
       <nav className="bg-white border-b w-full md:static md:text-sm md:border-none">
-        <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
+        <div className="items-center px-2 mx-auto md:flex md:px-6">
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <a href="#">
-                <p></p>
+                <p className="font-extrabold text-xl">Brainwave.io</p>
             </a>
             <div className="md:hidden">
               <button className="text-gray-500 hover:text-gray-800" onClick={() => setState(!state)}>
@@ -54,24 +57,8 @@ export default function Header() {
                   </li>
                 );
               })}
-              <span className="hidden w-px h-6 bg-gray-300 md:block"></span>
               <div className="space-y-3 items-center gap-x-6 md:flex md:space-y-0">
-                <li>
-                  <a
-                    href="#"
-                    className="block py-3 text-center text-gray-700 hover:text-indigo-600 border rounded-lg md:border-none"
-                  >
-                    Log in
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-3 px-4 font-medium text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 active:shadow-none rounded-lg shadow md:inline"
-                  >
-                    Sign in
-                  </a>
-                </li>
+                Get Started Now
               </div>
             </ul>
           </div>
