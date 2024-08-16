85a119d630233c5cac3eddae74f502bf5331868a
AxelSalim <adjakidjememiaghe@gmail.com>
2024-08-16 10:22:52 +0100

    modification de la structure

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,72 +1,17 @@
 import Image from "next/image";
 import { useEffect, useState } from 'react';
+import { Navbar } from "./Navbar";
 
 
 export default function Header() {
 
-    const [state, setState] = useState<boolean>(false);
-    
-    
-    const navigation = [
-      { title: "Demos", path: "#" },
-      { title: "Pages", path: "#" },
-      { title: "Support", path: "#" },
-      { title: "Contact", path: "#" },
-    ];
-
-
   return (
-    <section >
-        <section className="relative h-[95%]">
-            <Image src="/1x/''Fichier 6.png" width={2000} height={2000} alt="Banner Image" className="absolute object-cover"/>
+    <section className="relative">
+        <section className=" h-[95%]">
+            <Image src="/1x/''Fichier 6.png" width={1000} height={1000} alt="Banner Image" className="absolute w-full object-cover inset-0 z-[-1]"/>
         </section>
-      
-      <nav className="z-20 absolute inset-0 border-b w-full md:static md:text-sm md:border-none">
-        <div className="items-center px-2 mx-auto md:flex md:px-6">
-          <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <a href="#">
-                <p className="font-extrabold text-xl">Brainwave.io</p>
-            </a>
-            <div className="md:hidden">
-              <button className="text-gray-500 hover:text-gray-800" onClick={() => setState(!state)}>
-                {state ? (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
-          <div
-            className={`flex-1 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              state ? "block" : "hidden"
-            }`}
-          >
-            <ul className="justify-end items-center space-y-6 md:flex md:space-x-6 md:space-y-0">
-              {navigation.map((item, idx) => {
-                return (
-                  <li key={idx} className="text-gray-700 hover:text-indigo-600">
-                    <a href={item.path} className="block">
-                      {item.title}
-                    </a>
-                  </li>
-                );
-              })}
-              <div className="space-y-3 font-bold items-center gap-x-6 md:flex md:space-y-0">
-                Get Started Now
-              </div>
-            </ul>
-          </div>
-        </div>
-      </nav>
+
+        <Navbar></Navbar>
     </section>
   );
 }
--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -8,21 +8,125 @@ export default function main() {
         <>
 
         {/* section */}
-        <article className="relative">
-                        
-            {/* <section className="mx-auto ">
-                <div className="md:flex max-w-lg">
-                    <div className="w-20 h-20 mx-auto mb-6 md:mr-6 flex-shrink-0">
-                        <Image src="/1x/''Fichier 38.png" width={100} height={100} alt="Image background" className=" w-full object-cover mt-10 z-10" />
+        <article className="relative bg-white pb-40">
+            <section className="flex flex-col items-center relative  text-center border-0 border-gray-200 my-40">
+                <h2  className="box-border m-0 font-extrabold text-2xl md:text-4xl">How does it work ?</h2>
+                <p className="text-black">With lost of unique blocks, you can easily build a page easily without any coding.</p>
+
+                <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+                    <div className="max-w-4xl mx-auto text-center absolute z-10 w-full hidden md:block">
+                        <div className="border-4 w-full border-gray-300 border-dashed top-1/2 left-1/2"></div>
                     </div>
-                        
-                    <div className="flex-grow text-center md:text-left">
-                        <p className="font-bold">Senior Developer</p>
-                        <h3 className="text-xl heading">John Doe</h3>
-                        <p className="mt-2 mb-3">John is a Senior Developer, mainly works in backend technologies.</p>
+                    
+                    <div className="flex flex-col items-center z-20">
+                        <div className="bg-red-500 text-white rounded-full w-16 h-16 text-2xl font-bold flex items-center justify-center mb-2">1</div>
+                        <h3 className="font-semibold">Install App</h3>
+                        <p className="text-muted-foreground">With lots of unique blocks, you can easily build a page easily without any coding.</p>
+                    </div>
+
+                    <div className="flex flex-col items-center z-20">
+                        <div className="bg-green-500 text-white rounded-full w-16 h-16 text-2xl font-bold flex items-center justify-center mb-2">2</div>
+                        <h3 className="font-semibold">Add Team Members</h3>
+                        <p className="text-muted-foreground">With lots of unique blocks, you can easily build a page easily without any coding.</p>
+                    </div>
+
+                    <div className="flex flex-col items-center z-20">
+                        <div className="bg-blue-500 text-white rounded-full w-16 h-16 text-2xl font-bold flex items-center justify-center mb-2">3</div>
+                        <h3 className="font-semibold">Start Rolling!</h3>
+                        <p className="text-muted-foreground">With lots of unique blocks, you can easily build a page easily without any coding.</p>
                     </div>
                 </div>
-            </section> */}
+            </section>
+        </article>
+
+        {/* section */}
+        <article className="relative ">
+            <Image src="/1x/''Fichier 37.png" width={2000} height={2000} alt="Image background" className=" w-full object-cover z-30" />
+            <Image src="/1x/''Fichier 36.png" width={2500} height={2500} alt="Image background" className="w-full absolute -z-10" />
+            
+            <section className="relative mx-auto px-4 md:px-8 text-center flex justify-center -translate-y-1/2">
+                <div className="z-40">
+                    <Image src="/1x/''Fichier 33.png" width={80} height={80} alt="Image background" className="bg-cover absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+                </div>
+                <div className="z-30">
+                    <Image src="/1x/Fichier 81.png" width={900} height={50} alt="Image background" className="bg-cover rounded-3xl" />
+                </div>
+                <div className="z-20">
+                    <Image src="/1x/''Fichier 59.png" width={120} height={120} alt="Image background" className="bg-cover absolute translate-x-48 translate-y-7 bottom-0 left-0" />
+                </div>
+            </section>
+            
+            <section className="flex flex-col items-center  text-center border-0 mb-4 border-gray-200">
+                <h2  className="w-1/2 font-semibold text-xl md:text-4xl text-white">We made this app to solve your problems.</h2>
+            </section>
+
+            <section className="mx-auto max-w-4xl w-fit px-4 md:px-8 text-[#161c2d] grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-16 mb-5">
+                <div className="md:flex max-w-lg">
+                    <div className="w-20 h-20 mx-auto mt-1">
+                        <Image src="/1x/''Fichier 38.png" width={70} height={80} alt="Image background" className=" object-cover z-10" />
+                    </div>
+                        
+                    <div className="flex-grow text-center md:text-left ml-3">
+                        <p className="font-bold text-white">Unlimited Projects</p>
+                        <p className="mt-2 mb-3 text-white text-sm font-extralight">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+                    </div>
+                </div>
+
+                <div className="md:flex max-w-lg">
+                    <div className="w-20 h-20 mx-auto mt-1">
+                        <Image src="/1x/''Fichier 39.png" width={80} height={80} alt="Image background" className=" object-cover z-10" />
+                    </div>
+                        
+                    <div className="flex-grow text-center md:text-left ml-3">
+                        <p className="font-bold text-white">Team Management</p>
+                        <p className="mt-2 mb-3 text-white text-sm font-extralight">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+                    </div>
+                </div>
+
+                <div className="md:flex max-w-lg">
+                    <div className="w-20 h-20 mx-auto mt-1">
+                        <Image src="/1x/''Fichier 40.png" width={80} height={80} alt="Image background" className=" object-cover z-10" />
+                    </div>
+                        
+                    <div className="flex-grow text-center md:text-left ml-3">
+                        <p className="font-bold text-white">File Sharing</p>
+                        <p className="mt-2 mb-3 text-white text-sm font-extralight">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+                    </div>
+                </div>
+
+                <div className="md:flex max-w-lg">
+                    <div className="w-20 h-20 mx-auto mt-1">
+                        <Image src="/1x/''Fichier 41.png" width={80} height={80} alt="Image background" className=" object-cover z-10" />
+                    </div>
+                        
+                    <div className="flex-grow text-center md:text-left ml-3">
+                        <p className="font-bold text-white">Video Meetings</p>
+                        <p className="mt-2 mb-3 text-white text-sm font-extralight">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+                    </div>
+                </div>
+
+                <div className="md:flex max-w-lg">
+                    <div className="w-20 h-20 mx-auto mt-1">
+                        <Image src="/1x/''Fichier 42.png" width={80} height={80} alt="Image background" className=" object-cover z-10" />
+                    </div>
+                        
+                    <div className="flex-grow text-center md:text-left ml-3">
+                        <p className="font-bold text-white">Time Tracking</p>
+                        <p className="mt-2 mb-3 text-white text-sm font-extralight">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+                    </div>
+                </div>
+
+                <div className="md:flex max-w-lg">
+                    <div className="w-20 h-20 mx-auto mt-1">
+                        <Image src="/1x/''Fichier 44.png" width={80} height={80} alt="Image background" className=" object-cover z-10" />
+                    </div>
+                        
+                    <div className="flex-grow text-center md:text-left ml-3">
+                        <p className="font-bold text-white">Payment System</p>
+                        <p className="mt-2 mb-3 text-white text-sm font-extralight">With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
+                    </div>
+                </div>
+            </section>
 
             <section className="max-w-md mx-auto text-center w-full relative flex justify-center my-28">
                 <div className="">
@@ -32,7 +136,7 @@ export default function main() {
             
             <section className="max-w-xl mx-auto text-[#161c2d]">
                 <section className="max-w-md mx-auto text-center w-full relative flex justify-center my-10">
-                    <p className="font-bold text-xl">1,749 remote teams have shared their experience with our app!</p>
+                    <p className="font-bold text-white text-xl">1,749 remote teams have shared their experience with our app!</p>
                     <div className="absolute inset-6 -space-y-10">
                         <Image src="/1x/''Fichier 46.png" width={100} height={100} alt="Image background" className="ml-2 w-2/4" />
                     </div>
@@ -75,7 +179,7 @@ export default function main() {
                     </div>
                 </section>
 
-                <section className="flex justify-center items-center gap-2 text-center py-10 mb-20 ">
+                <section className="flex justify-center items-center gap-2 text-center py-10  ">
                     <p className="text-[#68d585] text-semibold">Read more reviews</p>
                     <div className="flex items-center text-[#68d585] cursor-pointer hover:scale-110 transition-all">
                         <FaArrowRight className="w-3 h-3"/>
@@ -83,20 +187,20 @@ export default function main() {
                 </section>
             </section>
 
-            {/* <Image src="/1x/''Fichier 36.png" width={800} height={1200} alt="Image background" className="w-full -z-10" /> */}
+            
         </article>
 
         {/* section */}
          <article className="bg-[#f4f7fa]">
             <section className="py-6 leading-7 text-[#161c2d] sm:py-12 md:py-16">
                 <div className="box-border px-4 mx-auto sm:px-6 md:px-6 lg:px-0 max-w-7xl">
-                    <div className="flex flex-col items-center leading-7 text-center border-0 border-gray-200">
-                        <h2 id="pricing" className="box-border m-0 font-extrabold text-2xl md:text-4xl">Pricing made easy</h2>
-                        <p className="mt-5 text-md font-light ">With lots of unique blocks, you can easily build a page easily without any coding.</p>
+                    <div className="flex flex-col items-center leading-7 text-center border-0 border-gray-200 pt-5">
+                        <h2 className="box-border m-0 font-extrabold text-2xl md:text-4xl">Pricing made easy</h2>
+                        <p className="mt-5 w-1/3 text-md font-light ">With lots of unique blocks, you can easily build a page easily without any coding.</p>
                     </div>
                 </div>
 
-                <div className="max-w-5xl mx-auto grid grid-cols-1 gap-4 mt-4 border-0 border-gray-200 sm:mt-6 sm:gap-6 md:mt-8 lg:grid-cols-2">
+                <div className="max-w-5xl mx-auto grid grid-cols-1 gap-4 mt-8 border-0 border-gray-200 sm:mt-6 sm:gap-6 md:mt-8 lg:grid-cols-2">
                     
                     {/* Price 1  */}
                     <div className="w-3/4 flex flex-col items-center max-w-md p-4 mx-auto my-0 border rounded-lg">
